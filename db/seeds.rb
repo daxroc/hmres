056d69f575a67d1c0950880b55b5da4e5595f1fc
Damien Roche <dcroche@gmail.com>
2015-04-11 14:33:08 +0000

    Added 50 users to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+fnames = ['john','jane','jim','jenifer','paul','paula']
+snames = ['murphy','slater','hayden','carrol','sinclare']
+sex = ['M','F']
+
+50.times do |i|
+  Employee.create(
+    fname: fnames[rand(0..fnames.size)],
+    sname: snames[rand(0..snames.size)],
+    sex: sex[rand(0..1)],
+    dob: "#{rand(1945..1985)}-#{rand(1..12)}-#{rand(1..27)}",
+    join_date: "#{rand(1998..2014)}-#{rand(1..12)}-#{rand(1..27)}",
+    salary: 12000 * rand(1..10)
+  )
+end
